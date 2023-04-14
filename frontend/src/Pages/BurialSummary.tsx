040b2f3c481ced5ee0ef192780aec32285a322d0
Blake <blake.pereira2018@gmail.com>
2023-04-14 12:06:15 -0600

    bill

--- frontend/src/Pages/BurialSummary.tsx
+++ frontend/src/Pages/BurialSummary.tsx
@@ -2,50 +2,13 @@ import React, { useState } from "react";
 import teams from "../Data.json";
 import MyBody from "../Components/MyBody";
 import MyTable from "../Components/Table";
-
-const Teams = teams.teams;
-
-interface TeamData {
-  tid: number;
-  cid: number;
-  did: number;
-  school: string;
-  name: string;
-  abbrev: string;
-  pop: number;
-  city: string;
-  state: string;
-  latitude: number;
-}
+import { Box } from "@mui/material";
 
 const DataTable: React.FC = () => {
-  // removed prop parameter
-  const [currentPage, setCurrentPage] = useState<number>(1);
-  const [itemsPerPage, setItemsPerPage] = useState<number>(10);
-  const [searchQuery, setSearchQuery] = useState<string>("");
-
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-
-  const filteredData = Teams.filter((team) =>
-    team.name.toLowerCase().includes(searchQuery.toLowerCase())
-  );
-
-  const currentItems = filteredData.slice(indexOfFirstItem, indexOfLastItem);
-
-  const pageNumbers = [];
-  for (let i = 1; i <= Math.ceil(filteredData.length / itemsPerPage); i++) {
-    pageNumbers.push(i);
-  }
-
-  const handlePaginationClick = (pageNumber: number) => {
-    setCurrentPage(pageNumber);
-  };
-
   return (
-    <MyBody>
+    <div style={{ overflowX: "auto" }}>
       <MyTable />
-    </MyBody>
+    </div>
   );
 };
 
